38247f002a3f79af2ea0d0994df9cb269e2ae49e
Tom Paton <thomas_paton@hotmail.co.uk>
2023-04-18 19:23:56 +0100

    remove dupliate message

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,6 @@ db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
-login.login_message = 'Please log in to access this page.'
 bootstrap = Bootstrap()
 
 
